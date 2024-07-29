4d5b24d6f12cdf63b0b8aa2851ad02d74efe7553
pvannewar <144586972+pvannewar@users.noreply.github.com>
2024-07-30 00:39:04 +0530

    Add files via upload

---
+++ jsLec2Variable.js
@@ -0,0 +1,10 @@
+// defind primitive data type
+//nn bb ss u
+let a=null;
+let b=345;
+let c=true;
+let d=BigInt(345);
+let e="prajwal";
+let f=Symbol("i am fine");
+let g=undefined;
+console.log(a,b,c,d,e,f,g)
